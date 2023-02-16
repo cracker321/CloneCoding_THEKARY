4757c98459acae5d65e4c24bc9c75b7b3b87af0e
unknown <joe@thekary.com>
2023-02-16 16:04:03 +0900

    yujong

---
+++ src/main/java/com/example/thekary/clonecoding/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.example.thekary.clonecoding.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/home")
+    public String mainPage(){
+
+        return "home.html";
+    }
+
+}
---
+++ src/test/java/com/example/thekary/clonecoding/ClonecodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thekary.clonecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClonecodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
